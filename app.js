a1795ff2ad261ae7242632ee8a9fbe45560006c7
Igor Rozhkov <zawert23879@gmail.com>
2024-06-11 13:25:23 +0300

    qq

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that! FRONT');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
